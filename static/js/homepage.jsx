e567aa2c1771f972a194d77b4493ec621a379043
Michelle Chen <mich.chen.94@gmail.com>
2020-07-31 13:46:15 -0700

    fixed requirements.txt

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -3,11 +3,11 @@
 function Homepage() {
   return (
   //use React.Fragment to not overload page with extra elements
-    <div>
+    <React.Fragment>
       <p>Hello Friend! Come check out Balloonicorn's Trading site!</p>
       <a href="/cards">Click here to view the trading cards.</a>
       <img src="/static/img/balloonicorn.jpg" />
-    </div>
+    </React.Fragment>
   );
 }
 
